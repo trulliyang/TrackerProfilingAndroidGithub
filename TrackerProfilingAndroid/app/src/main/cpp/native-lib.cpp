572917875c14a601c8b6314d2aef9d501b418f7d
shiyang <trulliyang@126.com>
2017-08-23 21:11:06 +0800

    [shiyang] we can use arcsoft

--- TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
+++ TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
@@ -1,8 +1,8 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-//#include <amcomdef.h>
-//#include <arcsoft_spotlight.h>
+#include <amcomdef.h>
+#include <arcsoft_spotlight.h>
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
@@ -15,12 +15,12 @@ Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
 extern "C"
 JNIEXPORT void JNICALL
 Java_appmagics_trackerprofilingandroid_Tracker_initJNI(JNIEnv *env, jobject instance, jint type) {
-//    MRECT *_facerect = new MRECT();
-//    _facerect->bottom = 1;
-    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang jni init type2 = %d", type);
-//    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang test bottom = %d", _facerect->bottom);
-//    delete(_facerect);
-//    _facerect = nullptr;
+    MRECT *_facerect = new MRECT();
+    _facerect->bottom = 1;
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang jni init type = %d", type);
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang test bottom = %d", _facerect->bottom);
+    delete(_facerect);
+    _facerect = nullptr;
 
-//    MHandle mAsHandle = ASL_CreateEngine();
+    MHandle mAsHandle = ASL_CreateEngine();
 }
