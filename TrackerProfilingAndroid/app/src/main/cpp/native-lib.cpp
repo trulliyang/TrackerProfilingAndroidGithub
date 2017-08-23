a6fc085bf5dd561e5c4896b49260092c3f1f68bd
shiyang <trulliyang@126.com>
2017-08-23 21:09:39 +0800

    [shiyang] ok now

--- TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
+++ TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,8 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-
+//#include <amcomdef.h>
+//#include <arcsoft_spotlight.h>
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
@@ -14,5 +15,12 @@ Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
 extern "C"
 JNIEXPORT void JNICALL
 Java_appmagics_trackerprofilingandroid_Tracker_initJNI(JNIEnv *env, jobject instance, jint type) {
-    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni", "shiyang jni init type = %d", type);
+//    MRECT *_facerect = new MRECT();
+//    _facerect->bottom = 1;
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang jni init type2 = %d", type);
+//    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang test bottom = %d", _facerect->bottom);
+//    delete(_facerect);
+//    _facerect = nullptr;
+
+//    MHandle mAsHandle = ASL_CreateEngine();
 }
