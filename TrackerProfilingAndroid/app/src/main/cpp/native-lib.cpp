b719c6e85ccf93fac550317938676a49590e7587
shiyang <trulliyang@126.com>
2017-08-24 14:53:58 +0800

    [shiyang] more parameter

--- TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
+++ TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
@@ -3,6 +3,15 @@
 #include <android/log.h>
 #include <amcomdef.h>
 #include <arcsoft_spotlight.h>
+
+std::string jstringTostring(JNIEnv *env, jstring jstr) {
+    const char *c_str = NULL;
+    c_str = env->GetStringUTFChars(jstr, NULL);
+    std::string stemp(c_str);
+    env->ReleaseStringUTFChars(jstr, c_str);
+    return stemp;
+}
+
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
@@ -14,13 +23,54 @@ Java_appmagics_trackerprofilingandroid_MainActivity_stringFromJNI(
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_appmagics_trackerprofilingandroid_Tracker_initJNI(JNIEnv *env, jobject instance, jint type) {
+Java_appmagics_trackerprofilingandroid_Tracker_initJNI(
+        JNIEnv *env, jobject instance, jint type) {
     MRECT *_facerect = new MRECT();
     _facerect->bottom = 1;
-    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang jni init type = %d", type);
-    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "shiyang test bottom = %d", _facerect->bottom);
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni", "shiyang jni init type = %d", type);
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni", "shiyang test bottom = %d", _facerect->bottom);
     delete(_facerect);
     _facerect = nullptr;
 
     MHandle mAsHandle = ASL_CreateEngine();
+    if (!mAsHandle) {
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "arcsoft create engine fail\n");
+//        return false;
+    }
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_appmagics_trackerprofilingandroid_Tracker_initJNI2(
+        JNIEnv *env, jobject instance, jobject context, jstring respath_,
+        jint imgw, jint imgh, jint formate, jint img_angle, jint type) {
+    MRECT *_facerect = new MRECT();
+    _facerect->bottom = 1;
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni", "shiyang jni init2 type = %d", type);
+    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni", "shiyang test bottom = %d", _facerect->bottom);
+    delete(_facerect);
+    _facerect = nullptr;
+
+    MHandle mAsHandle = ASL_CreateEngine();
+    if (!mAsHandle) {
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "arcsoft create engine fail\n");
+//        return false;
+    }
+    std::string resPath = jstringTostring(env, respath_);
+    MLong result = ASL_Initialize(mAsHandle, resPath.c_str(), ASL_MAX_FACE_NUM,
+                                  (MPVoid *) env, (void **) context);
+    if (result != MOK) {
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni2", "arcsoft init fail\n");
+//        return false;
+    }
+    //带不带美颜功能
+    //ASL_SetProcessModel(mAsHandle, ASL_PROCESS_MODEL_NONE);
+    ASL_SetProcessModel(mAsHandle, ASL_PROCESS_MODEL_FACEOUTLINE | ASL_PROCESS_MODEL_FACEBEAUTY);
+    ASL_SetFaceSkinSoftenLevel(mAsHandle, 100);
+    ASL_SetFaceBrightLevel(mAsHandle, 100);
+    //
+    int mAsPointNum = ASL_GetFaceOutlinePointCount();
+    int *mAsPointArray = new int[ASL_MAX_FACE_NUM * mAsPointNum * 2];
+    delete[] mAsPointArray;
+    mAsPointArray = nullptr;
 }
