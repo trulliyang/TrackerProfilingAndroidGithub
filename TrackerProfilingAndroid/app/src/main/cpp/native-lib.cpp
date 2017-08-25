230557cf16d66e5f17e9001f0c1fcf478ce9dd4b
shiyang <trulliyang@126.com>
2017-08-25 17:45:33 +0800

    no message

--- TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
+++ TrackerProfilingAndroid/app/src/main/cpp/native-lib.cpp
@@ -48,19 +48,24 @@ Java_appmagics_trackerprofilingandroid_Tracker_initJNI0(
 
     MHandle mAsHandle = ASL_CreateEngine();
     if (!mAsHandle) {
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft create engine fail\n");
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0",
+                            "shiyang arcsoft create engine fail\n");
 //        return false;
     } else {
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft create engine sucess\n");
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0",
+                            "shiyang arcsoft create engine sucess\n");
     }
     std::string resPath = jstringTostring(env, respath_);
 
-    FILE* pYUV = fopen("/storage/emulated/0/ws23.yuv", "r");
-    int len = 1280*720*3/2;
+    FILE *pYUV = fopen("/storage/emulated/0/ws23.yuv", "r");
+    int lenPerFrame = 1280 * 720 * 3 / 2;
+    int Frame = 50;
+    int len = lenPerFrame * Frame;
     unsigned char *buf = new unsigned char[len];
     if (pYUV) {
         __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang open yuv success\n");
         fread(buf, 1, len, pYUV);
+        fclose(pYUV);
     } else {
         __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang open yuv failed\n");
     }
@@ -69,7 +74,8 @@ Java_appmagics_trackerprofilingandroid_Tracker_initJNI0(
     MLong result = ASL_Initialize(mAsHandle, "/storage/emulated/0/track_data.dat", ASL_MAX_FACE_NUM,
                                   (MPVoid *) env, (void **) &context);
     if (result != MOK) {
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft init fail, result = %ld\n", result);
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0",
+                            "shiyang arcsoft init fail, result = %ld\n", result);
 //        return false;
     } else {
         __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft init suceess\n");
@@ -87,30 +93,40 @@ Java_appmagics_trackerprofilingandroid_Tracker_initJNI0(
     OffScreenIn.i32Width = imgw;
     OffScreenIn.i32Height = imgh;
 
-    OffScreenIn.pi32Pitch[0] = OffScreenIn.i32Width;
-    OffScreenIn.pi32Pitch[1] = OffScreenIn.i32Width;
-    OffScreenIn.ppu8Plane[0] = buf;
-    OffScreenIn.ppu8Plane[1] = buf + sizeof(unsigned char)*1280*720;
-    MInt32 nFaceCountInOut = ASL_MAX_FACE_NUM;
-    MRECT rcFaceRectOut[ASL_MAX_FACE_NUM];
-    MFloat faceOrientOut[ASL_MAX_FACE_NUM * 3];
-    MPOINT *pFaceOutlinePointOut = (MPOINT *) mAsPointArray;
-    __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang ready to arcsoft process\n");
-    MRESULT hr = ASL_Process(mAsHandle,
-                             &OffScreenIn,
-                             &OffScreenIn,
-                             &nFaceCountInOut,
-                             pFaceOutlinePointOut,
-                             rcFaceRectOut,
-                             faceOrientOut
-    );
-    if (hr != MOK) {
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft process fail\n");
-    } else {
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft process sucess\n");
-        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft face count = %d\n", nFaceCountInOut);
+
+    for (int i=0; i<Frame; i++) {
+        OffScreenIn.pi32Pitch[0] = OffScreenIn.i32Width;
+        OffScreenIn.pi32Pitch[1] = OffScreenIn.i32Width;
+        OffScreenIn.ppu8Plane[0] = buf + i*lenPerFrame;
+        OffScreenIn.ppu8Plane[1] = buf + i*lenPerFrame + sizeof(unsigned char) * 1280 * 720;
+        MInt32 nFaceCountInOut = ASL_MAX_FACE_NUM;
+        MRECT rcFaceRectOut[ASL_MAX_FACE_NUM];
+        MFloat faceOrientOut[ASL_MAX_FACE_NUM * 3];
+        MPOINT *pFaceOutlinePointOut = (MPOINT *) mAsPointArray;
+        __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang ready to arcsoft process\n");
+        MRESULT hr = ASL_Process(mAsHandle,
+                                 &OffScreenIn,
+                                 &OffScreenIn,
+                                 &nFaceCountInOut,
+                                 pFaceOutlinePointOut,
+                                 rcFaceRectOut,
+                                 faceOrientOut
+        );
+        if (hr != MOK) {
+            __android_log_print(ANDROID_LOG_ERROR, "shiyang jni0", "shiyang arcsoft process fail\n");
+        } else {
+            __android_log_print(
+                    ANDROID_LOG_ERROR,
+                    "shiyang jni0",
+                    "shiyang arcsoft process sucess\n"
+            );
+            __android_log_print(
+                    ANDROID_LOG_ERROR,
+                    "shiyang jni0",
+                    "shiyang arcsoft frame %d face count = %d\n", i, nFaceCountInOut
+            );
+        }
     }
-    fclose(pYUV);
     delete[] mAsPointArray;
     mAsPointArray = nullptr;
 }
